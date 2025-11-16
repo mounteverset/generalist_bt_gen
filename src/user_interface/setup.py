edc5c166fca24d5d3d6c9857ef54df868d012ffa
Lukas Evers <Lukas.E@web.de>
2025-11-16 23:32:20 +0100

    fixed the problem by downgrading setuptools to 79.0.1

--- src/user_interface/setup.py
+++ src/user_interface/setup.py
@@ -1,66 +1,29 @@
-from glob import glob
-from pathlib import Path
-import sys
-
 from setuptools import find_packages, setup
-
-with open('/tmp/user_interface_setup_args.log', 'a', encoding='utf-8') as f:
-    f.write(str(sys.argv) + '\n')
-
-def _strip_arg(flag: str):
-    while flag in sys.argv:
-        sys.argv.remove(flag)
-
-_strip_arg('--editable')
-_strip_arg('--uninstall')
-_strip_arg('--build-directory')
-
-if len(sys.argv) >= 2 and sys.argv[1] == 'develop':
-    new_args = [sys.argv[0], 'develop']
-    injected = False
-    default_install_dir = Path('build/user_interface/prefix_override')
-    for arg in sys.argv[2:]:
-        if arg == 'symlink_data':
-            continue
-        if arg.startswith('/') and not injected:
-            # skip original build directory path
-            continue
-        new_args.append(arg)
-    if not injected:
-        new_args.extend(['--install-dir', str(default_install_dir)])
-    sys.argv = new_args
-    with open('/tmp/user_interface_setup_args_filtered.log', 'a', encoding='utf-8') as f:
-        f.write(str(sys.argv) + '\n')
-
-from setuptools.command.develop import develop as _develop
-
-class DevelopCommand(_develop):
-    def check_site_dir(self):
-        return
-
+from glob import glob
 package_name = 'user_interface'
-share_dir = Path('share') / package_name
+
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    package_data={'user_interface': ['templates/*.html']},
-    include_package_data=True,
     data_files=[
         ('share/ament_index/resource_index/packages',
-         ['resource/' + package_name]),
-        (str(share_dir), ['package.xml', 'plan_chat_interface.md']),
-        (str(share_dir / 'config'), glob('config/*.yaml')),
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/config', glob('config/*.yaml')),
     ],
-    install_requires=[
-        'setuptools',
-        'rich',
-        'fastapi',
-        'uvicorn',
-        'jinja2',
-    ],
-    cmdclass={'develop': DevelopCommand},
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='luke',
+    maintainer_email='Lukas.E@web.de',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'console_scripts': [
             'chat_node = user_interface.chat_node:main',
